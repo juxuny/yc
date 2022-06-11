aa8022eeb72c38676045684c0a80c0327870759a
juxuny <juxuny@163.com>
2022-06-12 02:46:22 +0800

    post protobuf

--- curl/json.go
+++ curl/json.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/juxuny/yc/env"
 	"github.com/juxuny/yc/log"
+	"google.golang.org/protobuf/proto"
 	"io/ioutil"
 	"net/http"
 	"time"
@@ -76,6 +77,39 @@ func PostJSON(url string, param interface{}, out interface{}, headers ...map[str
 		logger.Debug(string(data))
 	}
 	err = json.Unmarshal(data, out)
+	if err != nil {
+		return -1, fmt.Errorf("unmrshal json erro: %s", err)
+	}
+	return resp.StatusCode, nil
+}
+
+func PostProtobuf(url string, param proto.Message, out proto.Message, headers ...map[string]interface{}) (code int, err error) {
+	d, _ := proto.Marshal(param)
+	body := bytes.NewReader(d)
+	req, err := http.NewRequest(http.MethodPost, url, body)
+	if err != nil {
+		return -1, fmt.Errorf("create request failed: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/protobuf")
+	if len(headers) > 0 {
+		for _, h := range headers {
+			for k, v := range h {
+				req.Header.Set(k, fmt.Sprint(v))
+			}
+		}
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return -1, fmt.Errorf("request error: %v", err)
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return -1, fmt.Errorf("read body error: %v", err)
+	}
+	if verbose {
+		logger.Debug(string(data))
+	}
+	err = proto.Unmarshal(data, out)
 	if err != nil {
 		return -1, fmt.Errorf("unmrshal json erro: %s", err)
 	}
